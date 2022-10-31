1debdc3db134404c86d6ece62a5eca9a6e7fe06c
Juwon-Yun <eooll2@daum.net>
2022-10-31 14:55:51 +0900

    - add: 머터리얼 탭바

--- lib/android/screens/android_main.dart
+++ lib/android/screens/android_main.dart
@@ -7,14 +7,75 @@ class AndroidMain extends StatefulWidget {
   State<AndroidMain> createState() => _AndroidMainState();
 }
 
-class _AndroidMainState extends State<AndroidMain> {
+class _AndroidMainState extends State<AndroidMain>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Image.asset('assets/images/mogi.png'),
+        body: TabBarView(
+          controller: _tabController,
+          children: const [
+            Center(
+              child: Text('home'),
+            ),
+            Center(
+              child: Text('search'),
+            ),
+            Center(
+              child: Text('camera'),
+            ),
+            Center(
+              child: Text('heart'),
+            ),
+            Center(
+              child: Text('settings'),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.grey,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.camera_alt,
+                  color: Colors.grey,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.grey,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
           ),
         ),
       ),
